8fbe10e4ade428275e796bd74d7a855136b23258
Adam Berecz <adam@laraform.io>
2022-07-14 18:30:39 +0200

    feat: rm composition api

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import './assets/main.css'
 
 import Vue from 'vue'
-import CompositionAPI from '@vue/composition-api'
 import Vueform from '@vueform/vueform/plugin'
 import vueformConfig from './../vueform.config'
 
 import App from './App.vue'
 
-Vue.use(CompositionAPI)
 Vue.use(Vueform, vueformConfig)
 
 new Vue({
